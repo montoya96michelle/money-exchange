f6e4c7eb3e94f6ac85b775e019551a0f40bc978a
michelle.96 <michelle96montoya@gmail.com>
2025-11-16 19:08:09 -0500

    First commit

---
+++ src/main/java/com/alura/moneyexchange/Main.java
@@ -0,0 +1,11 @@
+package com.alura.moneyexchange;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("¡Proyecto Conversor de Monedas funcionando!");
+
+	}
+
+}
